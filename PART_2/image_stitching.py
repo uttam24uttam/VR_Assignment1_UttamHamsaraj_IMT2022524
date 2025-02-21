5e1091e322509ae0ed33a6ef3e3341d3658059fc
Uttam <uttamhamsaraj24@gmail.com>
2025-02-21 14:47:12 +0530

    Update image_stitching.py

--- PART_2/image_stitching.py
+++ PART_2/image_stitching.py
@@ -4,18 +4,17 @@ import imutils
 import matplotlib.pyplot as plt
 import os
 
-
-image_paths=["images/left.jpg", "images/center.jpg", "images/right.jpg"]
-image_list=[cv2.imread(path) for path in image_paths]
+image_paths = ["images/left.jpg", "images/center.jpg", "images/right.jpg"]
+image_list = [cv2.imread(path) for path in image_paths]
 
 # Part A: Extracting Key Points 
 gray_images = [cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) for img in image_list]
 
 sift = cv2.SIFT_create()
-keypoints,descriptors = zip(*[sift.detectAndCompute(img, None) for img in gray_images])
+keypoints, descriptors = zip(*[sift.detectAndCompute(img, None) for img in gray_images])
 
 # Keypoints matching 
-FLANN_INDEX_KDTREE=1
+FLANN_INDEX_KDTREE = 1
 index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
 search_params = dict(checks=50)
 flann = cv2.FlannBasedMatcher(index_params, search_params)
@@ -23,15 +22,19 @@ flann = cv2.FlannBasedMatcher(index_params, search_params)
 matches12 = flann.knnMatch(descriptors[0], descriptors[1], k=2)
 matches23 = flann.knnMatch(descriptors[1], descriptors[2], k=2)
 
-good_matches12=[m for m, n in matches12 if m.distance < 0.75 * n.distance]
-good_matches23=[m for m, n in matches23 if m.distance < 0.75 * n.distance]
+good_matches12 = [m for m, n in matches12 if m.distance < 0.75 * n.distance]
+good_matches23 = [m for m, n in matches23 if m.distance < 0.75 * n.distance]
 
 os.makedirs("output_images", exist_ok=True)
 
-match_img12 = cv2.drawMatches(image_list[0], keypoints[0], image_list[1], keypoints[1], good_matches12, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+match_img12 = cv2.drawMatches(image_list[0], keypoints[0], image_list[1], keypoints[1], good_matches12, None, matchColor=(0, 255, 0), flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 cv2.imwrite("output_images/keypoint_matching_left_center.jpg", match_img12)
-match_img23 = cv2.drawMatches(image_list[1], keypoints[1], image_list[2], keypoints[2], good_matches23, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+match_img23 = cv2.drawMatches(image_list[1], keypoints[1], image_list[2], keypoints[2], good_matches23, None, matchColor=(0, 255, 0), flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 cv2.imwrite("output_images/keypoint_matching_center_right.jpg", match_img23)
+
+inv_gamma = 1.0 / 1.2
+table = np.array([(i / 255.0) ** inv_gamma * 255 for i in np.arange(0, 256)]).astype("uint8")
+image_list = [cv2.LUT(img, table) for img in image_list]
 
 cv2.imshow("Keypoint Matching (Left - Center)", match_img12)
 cv2.imshow("Keypoint Matching (Center - Right)", match_img23)
